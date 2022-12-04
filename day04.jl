3944c40fdac0e0b7922935ff8ad11e70ee17c1dc
Henrik Wolf <mail@henrikwolf.de>
2022-12-04 19:07:11 +0100

    add day4

--- day04.jl
+++ day04.jl
@@ -1,21 +1,41 @@
 include("executor.jl")
 
+## part one ##
 function day04(lines)
     matches = 0
 
-    # split(line, ";") -- String bei Zeichen ";" teilen. Beide Teile werden in einem Array gespeichert
-    # parse(Int, "200") -- Konvertiert die Zeichenkette "200" in den entsprechenden Zahlwert 200
-    # array[1] -- gibt den Wert bei Index 1 zurück
-    # Mathematische Operatoren: += (etwas zuaddieren); <= (kleiner-gleich); >= (größer-gleich); ==
-
     for line in lines
+        limits = split(line, [',', '-'])
+        limits = map(i -> parse(Int, i), limits)
 
-        if true == true
+        a_in_b = limits[1] <= limits[3] && limits[2] >= limits[4]
+        b_in_a = limits[1] >= limits[3] && limits[2] <= limits[4]
+
+        if (a_in_b || b_in_a)
+            matches += 1
         end
-
     end
 
     return matches
 end
 
-execute(4, day04)
+## part two ##
+function day04_2(lines)
+    matches = 0
+
+    for line in lines
+        limits = split(line, [',', '-'])
+        limits = map(i -> parse(Int8, i), limits)
+
+        a_above_b = limits[2] < limits[3]
+        b_above_a = limits[1] > limits[4]
+
+        if (a_above_b || b_above_a)
+            matches += 1
+        end
+    end
+
+    return length(lines) - matches
+end
+
+execute(4, day04_2)
